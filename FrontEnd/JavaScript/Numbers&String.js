68da7e30d6482d8fbdcc96ae2e08ae6f791ffe07
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-07-24 20:23:11 +0530

    Number

---
+++ FrontEnd/JavaScript/Numbers&String.js
@@ -0,0 +1,20 @@
+// Arithmetic Operator
+
+a=10;
+b=5;
+console.log(a+b);  //15
+console.log(a-b);  //5
+console.log(a*b);  //50
+console.log(a/b);  //2
+console.log(a%b);  //0
+
+no = 2.6;
+
+console.log(Math.round(no));  //3
+console.log(Math.ceil(no));  //3
+console.log(Math.floor(no)); //2
+console.log(Math.trunc(no)); //Remove only the decimal part  //2
+
+
+
+// Strings 
