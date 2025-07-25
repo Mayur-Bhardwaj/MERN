9bc2855ac384a6c36824acea6a494737e509a0dd
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-07-25 20:18:13 +0530

    TypeOfOperator

---
+++ FrontEnd/JavaScript/TypeOfOperator.js
@@ -0,0 +1,11 @@
+// TypeOf Operator:- Tells us thr data type of a variable.
+
+a = "Data";
+b = 10;
+c=true;
+d=3.4;
+
+console.log(a, typeof(a));   // Data string
+console.log(b, typeof(a));   // 10 string
+console.log(c, typeof(c));   // true boolean
+console.log(d, typeof(d));   // 3.4 number
