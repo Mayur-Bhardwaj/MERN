5e6a1b200c1f02aa861913f17e677d57b7066727
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-07-25 20:11:53 +0530

    Numbers & String

---
+++ FrontEnd/JavaScript/Assignment/Numbers&String.js
@@ -0,0 +1,31 @@
+// Arithmetic Operator
+
+a=10;
+b=5;
+console.log(a+b);  //15
+console.log(a-b);  //5
+console.log(a*b);  //50
+console.log(a/b);  //2
+console.log(a%b);  //0
+
+no = 2.6;
+
+console.log(Math.round(no));  //3
+console.log(Math.ceil(no));  //3
+console.log(Math.floor(no)); //2
+console.log(Math.trunc(no)); //Remove only the decimal part  //2
+
+
+
+// Strings :- Strings holds textual data, anything from a single character to paragraph.String can be defined through single quotes (" "), single quotes (' ') or Backticks (` `).
+
+console.log("This is Double Quotes String.");
+console.log('This is Single Quotes String.');
+console.log(`This is BackTick String. You're using this.`);
+
+console.log("JavScript " + "ReactJS");  //JavScript ReactJS    // String Concatination   
+
+// When we do concatination of string and number then it will be convert into the string
+
+console.log("You get the "+ 9 + " marks out of "+ 10); //You get the 9 marks out of 10
+console.log("You have got " + (1+1+1)+ " votes");  // You have got 3 votes
