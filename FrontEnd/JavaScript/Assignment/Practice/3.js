13e2f02150cd4f958a419ba131a2bf7d783abefb
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-08-10 17:08:03 +0530

    Count Number

---
+++ FrontEnd/JavaScript/Assignment/Practice/3.js
@@ -0,0 +1,14 @@
+// Count Number from 1-10
+
+let count = 0;
+
+for(let i=0; i<=10; i++){
+    if(i%2!=0){
+       // console.log(i);
+        count++;
+    }
+}
+console.log(count);
+
+// Output:-
+// 5
