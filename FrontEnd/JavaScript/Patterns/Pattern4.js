474d952e7ccd73b950656d1a5d4e6b040835bc23
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-08-24 15:20:56 +0530

    Pattern 4

---
+++ FrontEnd/JavaScript/Patterns/Pattern4.js
@@ -0,0 +1,19 @@
+// *****
+// *****
+// *****
+
+// let str = "";
+// for(let i=1; i<=5; i++){
+//   str = str + "*";
+// }
+// console.log(str);
+
+// The above code print the ***** in 1 line we need to print it inside a loop.
+
+for(let repeat=1; repeat<=3; repeat++){
+  let str = "";
+for(let i=1; i<=5; i++){
+  str = str + "*";
+}
+console.log(str);
+}
