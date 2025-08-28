10e1d679f839ab81dd94decf9d4f222c9d1967f0
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-08-28 20:19:35 +0530

    Basic Function

--- FrontEnd/JavaScript/Assignment/Functions/Basic.js
+++ FrontEnd/JavaScript/Assignment/Functions/Basic.js
@@ -1,1 +1,19 @@
+// Sum of 2 number
+function Sum(){
+  let a=10;
+  let b=20;
+  let res = a+b;
+  console.log(res);
+}
+Sum();
+// 30
 
+// OR 
+
+function SumOf2Number(a,b){
+  let sum = a+b;
+  console.log(sum);
+}
+SumOf2Number(8,10);
+
+// 18
