5ddc2cc393f057ac5a69c0522c9a150bb0ccd06f
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-09-21 17:05:41 +0530

    Updated

--- FrontEnd/JavaScript/Assignment/Arrays/Matrix/2.column.js
+++ FrontEnd/JavaScript/Assignment/Arrays/Matrix/2.column.js
@@ -5,12 +5,17 @@ let arr = [
 ]
 // i= rows, j= columns
 for(let i=0; i<arr.length; i++){
-  for(let j=0; j<=arr[0].length; j++){
+  for(let j=0; j<arr[i].length; j++){
     if(i==j){
       console.log(arr[i][j]);
     }
   }
 }
+
+// Output
+// 10
+// 50
+// 90
 
 // Anti-diagonal (top-right to bottom-left)
 console.log("Anti-diagonal:");
@@ -21,3 +26,26 @@ for(let i = 0; i < arr.length; i++){
     }
   }
 }
+
+// Output:
+// Anti-diagonal:
+// 30
+// 50
+// 70
+
+// Print the number except diagonal
+// for(let i=0; i<arr.length; i++){
+//   for(let j=0; j<arr[i].length; j++){
+//     if(i!=j){
+//       console.log(arr[i][j]);
+//     }
+//   }
+// }
+
+// Output
+// 20
+// 30
+// 40
+// 60
+// 70
+// 80
