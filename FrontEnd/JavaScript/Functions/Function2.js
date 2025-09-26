9095409cf416de50c3f91937bac39e6865dc610b
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-09-26 16:17:21 +0530

    Function 2

---
+++ FrontEnd/JavaScript/Functions/Function2.js
@@ -0,0 +1,23 @@
+// function sum(a,b){  //parameter
+//   let result = a+b;
+//   console.log(result);
+// }
+// sum(10,20); // argument  //30
+
+// function greeting(name){
+//   console.log(`Namaste ${name}Script`);
+// }
+// greeting("Java");
+
+// // Output:
+// // Namaste JavaScript
+
+// Default Parameter
+
+function greeting(name = "Type"){
+  console.log(`Namaste ${name}Script`);
+}
+greeting();
+
+// Output:
+// Namaste TypeScript
