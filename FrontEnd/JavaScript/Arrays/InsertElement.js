130218ed426afe21ce5c811e055312c9e9be07cc
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-12-18 19:05:54 +0530

    Insert Element

--- FrontEnd/JavaScript/Arrays/InsertElement.js
+++ FrontEnd/JavaScript/Arrays/InsertElement.js
@@ -51,12 +51,55 @@
 
 // Or
 
-let arr = [10,20,30,40,50];
+// let arr = [10,20,30,40,50];
 
-// let index = 0;
-// let n = 5;
+// // let index = 0;
+// // let n = 5;
 
- arr.unshift(5);
-console.log(arr);
+//  arr.unshift(5);
+// console.log(arr);
 
 // Output:- [ 5, 10, 20, 30, 40, 50 ]
+
+
+// Or
+
+// let arr = [10,20,30,40,50];
+
+// let index = 3;
+// let n = 35;
+// let brr = [];
+
+// for(let i=0; i<=arr.length-1; i++){
+//   if(index == i){
+//     brr.push(n);
+//   }
+//   brr.push(arr[i]);
+// }
+
+// console.log(brr);
+
+// Output: [ 10, 20, 30, 35, 40, 50 ]
+
+// Take an arraay of length 10 and insert an element 75 at position 8.
+
+let arr = [10,20,30,40,50,60,70,80,90,100];
+
+let n = 75;
+let index = 8;
+let brr = [];
+
+for(let i=0; i<=arr.length-1; i++){
+  if(i === index){
+    brr.push(n);
+  }
+  brr.push(arr[i]);
+ 
+}
+ console.log(brr);
+
+//  Output:- [
+//    10, 20, 30, 40, 50,
+//    60, 70, 80, 75, 90,
+//   100
+// ]
